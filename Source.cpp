9b7ada4f26ab3cad87541a4688754006c7a0f828
Vadim <53124674+mm0ri@users.noreply.github.com>
2021-03-26 14:50:50 +0300

    Source file were updated.
    Linux source file.

--- Source.cpp
+++ Source.cpp
@@ -12,3 +12,5 @@ int main()
 	return 0;
 }
 
+// To start programm on linux use this command in terminal.
+// cd "/home/vadim/Code/VisualStudio/CPP Projects/Example one/" && g++ Source.cpp Menu.cpp Functions.cpp -o Source &&dio/CPP Projects/Example one/"Source
